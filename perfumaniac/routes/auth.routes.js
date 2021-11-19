9628104d33997903bd46090ba8a555ed80ab22ad
Joana Cruz <joana_c_ca@hotmail.com>
2021-11-19 14:28:32 +0000

    add get route for sing-up

--- perfumaniac/routes/auth.routes.js
+++ perfumaniac/routes/auth.routes.js
@@ -1,5 +1,9 @@
 const router = required("express").Router();
 
 //routes
+// GET  /signup
+router.get("/signup", (req, res) => {
+    res.render("auth/signup-form");
+  });
 
 module.exports = router;
